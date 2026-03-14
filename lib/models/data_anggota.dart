4e88a964e7fdd92796ee6a3ab57c90460cfe2f22
unknown <itopfajar@gmail.com>
2026-03-14 21:28:47 +0700

    daftar kelompok page (kurang foto)

--- lib/models/data_anggota.dart
+++ lib/models/data_anggota.dart
@@ -2,23 +2,18 @@ import 'anggota_model.dart';
 
 final List<AnggotaKelompok> dataKelompok = [
   AnggotaKelompok(
-    nama: "Hiero Purbandono",
-    nim: "123230086",
-    foto: "assets/images/hiero.jpg",
+    nama: "Ahmad Akbar",
+    nim: "123230113",
+    foto: "",
   ),
   AnggotaKelompok(
-    nama: "Martin Aji Nugraha",
-    nim: "123230092",
-    foto: "assets/images/martin.jpg",
+    nama: "Vincentius Ariell Sorongan",
+    nim: "123230131",
+    foto: "",
   ),
   AnggotaKelompok(
-    nama: "Faisal Dani Noto Legowo",
-    nim: "123230097",
-    foto: "assets/images/faisal.jpg",
-  ),
-  AnggotaKelompok(
-    nama: "Muhammad Furaihan Al Harits",
-    nim: "123230108",
-    foto: "assets/images/furaihan.jpg",
-  ),
+    nama: "Isyraf Fajar Arundaya",
+    nim: "123230140",
+    foto: "",
+  )
 ];
